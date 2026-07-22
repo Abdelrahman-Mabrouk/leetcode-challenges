2ec29b7497f8ed5a7df6ec1c5d2b5131d3f08dcc
Abdelrahman  Mabrouk <125595697+Abdelrahman-Mabrouk@users.noreply.github.com>
2026-07-22 19:35:04 +0300

    Time: 0 ms (100%), Space: 12.8 MB (5.91%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -2,19 +2,18 @@ class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
       vector<int>result(n+m);
-    int i =0;
-        int j=0;
-        int l=0;
-        while(i<m && j<n){
-            if(nums1[i]<=nums2[j] ){result[l++]=nums1[i];i++;}
-            else if(nums1[i]>nums2[j]){result[l++]=nums2[j];j++;}
+    int i =m-1;
+        int j=n-1;
+        int l=n+m-1;
+        while(i>=0 && j>=0){
+            if(nums1[i]>=nums2[j] ){nums1[l--]=nums1[i];i--;}
+            else if(nums1[i]<nums2[j]){nums1[l--]=nums2[j];j--;}
         }
-        while(i<m){
-            result[l++]=nums1[i++];
+        while(i>=0){
+            nums1[l--]=nums1[i--];
         }
-    while(j<n){
-        result[l++]=nums2[j++];
+    while(j>=0){
+        nums1[l--]=nums2[j--];
     }
-        nums1=result;
     }
 };
