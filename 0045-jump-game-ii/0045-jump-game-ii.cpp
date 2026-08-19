5411b53821286d6f91a42fcb6191456abedff51a
Abdelrahman  Mabrouk <125595697+Abdelrahman-Mabrouk@users.noreply.github.com>
2026-08-19 10:32:55 +0300

    Time: 0 ms (100%), Space: 20.8 MB (29.32%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        int size = nums.size() , result = 0, mx = 0,i = 0,n=nums[0],idx=0;
-        if (size  == 1)return 0;
-        while(i<size) {
-            if(nums[i]+i>=mx){
-                mx= nums[i] + i;
-                idx = i;
+        int jumps = 0;
+        int currentEnd = 0;
+        int farthest = 0;
+        
+        // لا داعي للوصول للعنصر الأخير في اللوب لأننا لو وصلنا قبله يكفينا قفزة
+        for (int i = 0; i < nums.size() - 1; i++) {
+            farthest = max(farthest, i + nums[i]);
+            
+            // إذا وصلنا لنهاية المدى الحالي، يجب أن نقفز قفزة جديدة
+            if (i == currentEnd) {
+                jumps++;
+                currentEnd = farthest;
             }
-            i++;
-            if(i == n+1 && i!=size){
-                i =idx;
-                n = i  + nums[i] ;
-                result++;
-                mx= 0;
-            }
-
         }
-        return result+1;
-
+        
+        return jumps;
     }
 };
