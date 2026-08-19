8193d46fc0c70ce78ececdbe27086a04c5ee5394
Abdelrahman  Mabrouk <125595697+Abdelrahman-Mabrouk@users.noreply.github.com>
2026-08-19 10:33:06 +0300

    Time: 3 ms (28.34%), Space: 20.6 MB (54.38%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -5,11 +5,9 @@ public:
         int currentEnd = 0;
         int farthest = 0;
         
-        // لا داعي للوصول للعنصر الأخير في اللوب لأننا لو وصلنا قبله يكفينا قفزة
         for (int i = 0; i < nums.size() - 1; i++) {
             farthest = max(farthest, i + nums[i]);
             
-            // إذا وصلنا لنهاية المدى الحالي، يجب أن نقفز قفزة جديدة
             if (i == currentEnd) {
                 jumps++;
                 currentEnd = farthest;
