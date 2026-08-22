0daba796ee4d9459ed8bc3ba110ec5213867405d
Abdelrahman  Mabrouk <125595697+Abdelrahman-Mabrouk@users.noreply.github.com>
2026-08-22 08:58:05 +0300

    Time: 0 ms (100%), Space: 112.3 MB (99.52%) - LeetHub

--- 0134-gas-station/0134-gas-station.cpp
+++ 0134-gas-station/0134-gas-station.cpp
@@ -1,36 +1,26 @@
 class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-        int res = 0;
-        long long sum =0 , sum2=0;
-        bool flag =false;
-        vector<int> deff ;
-        for (int i = 0; i < gas.size(); ++i) {
-            sum+=gas[i];
-            sum2+=cost[i];
-            deff.push_back(gas[i]-cost[i]);
+        int total = 0;
+        int tank = 0;
+        int start;
+
+        for(int i=0;i<gas.size();i++){
+            int gain = gas[i] - cost[i];
+
+            total += gain;
+            tank += gain;
+
+            if(tank < 0){
+                start = i+1;
+                tank = 0;
+            }
         }
-        if(sum-sum2<0) return -1;
-        sum =deff[0];
-        int startIndex =0;
-        int i =0;
-        if (sum > 0 ) i =1;
-        for ( ; i < gas.size(); ++i) {
-            if(sum<0){
-                sum=0;
-                flag =true;
-                i-=1;
-                continue;}
 
-                if(flag){
-                    res = i;
-                    flag =false;
-                }
-               sum=deff[i]+sum;
-
+        if(total < 0){
+            return -1;
         }
-        if(flag)return gas.size()-1;
 
-        return res;
+        return start;
     }
 };
