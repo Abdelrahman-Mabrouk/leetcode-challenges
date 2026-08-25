1779f2e8f072acc994c785d35cab1f8955b4bf82
Abdelrahman  Mabrouk <125595697+Abdelrahman-Mabrouk@users.noreply.github.com>
2026-08-25 10:18:13 +0300

    Time: 0 ms (100%), Space: 68 MB (18.52%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -1,34 +1,15 @@
 class Solution {
 public:
     int candy(vector<int>& ratings) {
-        int n = ratings.size();
-        if (n <= 1) return n;
-
-        int res = 1;
-        int up = 1;
-        int down = 1;
-        int peak = 0;
-
-        for (int i = 1; i < n; ++i) {
-            if (ratings[i] > ratings[i - 1]) {
-                down = 1;
-                up++;
-                peak = up;
-                res += up;
-            }
-            else if (ratings[i] == ratings[i - 1]) {
-                up = 1;
-                down = 1;
-                peak = 0;
-                res += 1;
-            }
-            else {
-                up = 1;
-                down++;
-                res +=  down - (down <= peak ? 1 : 0);
-            }
+        int res = 1, up = 0, down = 0, peak = 0;
+        for (int i = 1; i < ratings.size(); ++i) {
+            if (ratings[i] > ratings[i - 1])
+                res += 1 + (++up), peak = up, down = 0;
+            else if (ratings[i] == ratings[i - 1])
+                res += 1, up = down = peak = 0;
+            else
+                res += 1 + (++down) - (down <= peak), up = 0;
         }
-
         return res;
     }
 };
