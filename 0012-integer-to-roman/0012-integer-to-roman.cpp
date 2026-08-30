da91a898859ec9928cf19fc41ff0e4660cdd9b05
Abdelrahman  Mabrouk <125595697+Abdelrahman-Mabrouk@users.noreply.github.com>
2026-08-30 14:13:29 +0300

    Time: 5 ms (38.38%), Space: 12.8 MB (17.65%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cpp
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -1,37 +1,24 @@
+#include <string>
+#include <vector>
+
 class Solution {
 public:
-    string intToRoman(int n) {
-        unordered_map<int,char> m{{1,'I'},{5,'V'},{10,'X'},{50,'L'},{100,'C'},{500,'D'},{1000,'M'}};
-        unordered_map<int,int> a{{1,1000},{2,500},{3,100},{4,50},{5,10},{6,5},{7,1}};
-        string res = "";
-        int len = log10(n),temp;
-        int k = 0,i=1;
-        while(n!=0){
-            temp = n / pow(10,len);
-            k=pow(10,len)*temp;
-            while(k < a[i] && k!=0) i++;
-
-            while(k >= a[i] && temp!=9 && temp!=4 && k!=0){
-                res+=m[a[i]];
-                k-=a[i];
-                if(k < a[i] && k!=0) i++;
+    std::string intToRoman(int num) {
+        const std::vector<std::pair<int, std::string>> roman = {
+            {1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"},
+            {100, "C"},  {90, "XC"},  {50, "L"},  {40, "XL"},
+            {10, "X"},   {9, "IX"},   {5, "V"},   {4, "IV"},
+            {1, "I"}
+        };
+        
+        std::string result = "";
+        for (const auto& [value, symbol] : roman) {
+            while (num >= value) {
+                result += symbol;
+                num -= value;
             }
-            if(temp==9 ){
-                res+=m[static_cast<int>(pow(10,len))];
-                res+=m[static_cast<int>(pow(10,len+1))];
-                k-=9 * pow(10,len);
-            }
-            if(temp==4){
-                res+=m[static_cast<int>(pow(10,len))];
-                res+=m[static_cast<int> (5*pow(10,len))];
-                k-=4*pow(10,len);
-            }
-            n %= static_cast<int> (pow(10,len));
-            len--;
-
         }
-
-        return res;
-
+        
+        return result;
     }
-    };
+};
