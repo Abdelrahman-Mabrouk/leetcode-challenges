bd56e0c35ad209b6246ad0d0d3f556c7abe76c63
Abdelrahman  Mabrouk <125595697+Abdelrahman-Mabrouk@users.noreply.github.com>
2026-08-30 14:22:00 +0300

    Time: 8 ms (18.13%), Space: 16.2 MB (5.12%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cpp
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -1,24 +1,20 @@
-#include <string>
-#include <vector>
-
 class Solution {
 public:
-    std::string intToRoman(int num) {
-        const std::vector<std::pair<int, std::string>> roman = {
-            {1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"},
-            {100, "C"},  {90, "XC"},  {50, "L"},  {40, "XL"},
-            {10, "X"},   {9, "IX"},   {5, "V"},   {4, "IV"},
-            {1, "I"}
+    string intToRoman(int n) {
+        map<int, string> m = {
+                {1, "I"}, {4, "IV"}, {5, "V"}, {9, "IX"}, {10, "X"},
+                {40, "XL"}, {50, "L"}, {90, "XC"}, {100, "C"},
+                {400, "CD"}, {500, "D"}, {900, "CM"}, {1000, "M"}
         };
-        
-        std::string result = "";
-        for (const auto& [value, symbol] : roman) {
-            while (num >= value) {
-                result += symbol;
-                num -= value;
+
+        string res = "";
+        for (auto i = m.rbegin(); i != m.rend(); ++i) {
+            while (n >= i->first) {
+                res += i->second;
+                n -= i->first;
             }
         }
-        
-        return result;
+        return res;
+
     }
-};
+    };
